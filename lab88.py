3031d605001015766be9c54bf574f93411fef465
MOLDIR <moldirseitzhanova@gmail.com>
2022-11-09 11:27:41 +0600

    reduce functional

--- lab88.py
+++ lab88.py
@@ -3,3 +3,19 @@ def outer_func():
         print("Hello, World!")
 
     inner_func()
+
+
+def reducer_func(el_prev, el):
+    # el_prev — предшествующий элемент
+    # el — текущий элемент
+    return el_prev + el
+
+    # python 2
+
+
+reduce(reducer_func, [1, 2, 3])  # 6
+
+# python 3
+from functools import reduce
+
+reduce(reducer_func, [1, 2, 3])  # 6
